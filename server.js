5dcef77f1c9c660c33e75aae81a4b09b53deb2b7
francesjgonzales <francesj7837@gmail.com>
2024-02-26 19:43:14 -0700

    edited darabase url

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ app.use(express.static(publicDirectoryPath))
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to data base'))
 
-
+console.log(mongoose.connect(process.env.DATABASE_URL))
 
 // Start setting up your routes
 app.get('/', (req, res) => {
