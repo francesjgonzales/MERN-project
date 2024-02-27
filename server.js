28dc3e1c49e76c6d92076255a726a5bc9011b121
francesjgonzales <francesj7837@gmail.com>
2024-02-26 22:30:22 -0700

    fixing mongoose error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-/* require('dotenv').config(); */
-const dotenv = require('dotenv')
-dotenv.config({ path: __dirname + '/.env' })
+require('dotenv').config();
+const source = process.env.DATABASE_URL;
 
 const express = require('express')
 const app = express()
@@ -22,7 +21,8 @@ app.use(express.static(publicDirectoryPath))
 // Integrate Mongoose for MongoDB
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
+/* mongoose.connect(process.env.DATABASE_URL) */
+mongoose.connect(source)
 console.log(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
