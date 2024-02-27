e6f494cefcd6d22c32dd055b1a5fad73b6ffda7e
francesjgonzales <francesj7837@gmail.com>
2024-02-26 18:18:55 -0700

    added new file

--- server.js
+++ server.js
@@ -17,12 +17,15 @@ hbs.registerPartials(viewsPartialsPath)
 app.use(express.static(publicDirectoryPath))
 
 // Integrate Mongoose for MongoDB
+
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to data base'))
+
+
 
 // Start setting up your routes
 app.get('/', (req, res) => {
