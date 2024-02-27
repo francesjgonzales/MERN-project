2882aff41bc4c0ebffd1ca46aea2dffe7b2ba92e
francesjgonzales <francesj7837@gmail.com>
2024-02-26 20:06:04 -0700

    edited darabase url

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config({ path: path.resolve(__dirname, '/.env') });
 const express = require('express')
 const app = express()
 const hbs = require('hbs')
@@ -24,8 +25,6 @@ mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to data base'))
-
-console.log(mongoose.connect(process.env.DATABASE_URL))
 
 // Start setting up your routes
 app.get('/', (req, res) => {
