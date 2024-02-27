f263be996294de5a470a605f9928d7c887f4b4fd
francesjgonzales <francesj7837@gmail.com>
2024-02-26 18:57:54 -0700

    added new file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(publicDirectoryPath))
 
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('connected to data base'))
